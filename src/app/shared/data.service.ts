75e6e2a14462dedd16a7ebd6b98a81ffca6fe41a
Jafed Encinas <60824778+jafedencinas@users.noreply.github.com>
2021-03-24 12:00:52 -0600

    2nd update

--- src/app/shared/data.service.ts
+++ src/app/shared/data.service.ts
@@ -7,8 +7,8 @@ import { Todo } from './todo.model';
 export class DataService {
 
   todos: Todo[] = [
-    new Todo("This is a test!", true),
-    new Todo("Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil architecto atque ut assumenda cumque nostrum voluptates exercitationem aspernatur autem tempora reprehenderit aperiam praesentium eligendi, aut illo minima facere omnis nisi.")
+   // new Todo("This is a test!", true),
+   // new Todo("Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil architecto atque ut assumenda cumque nostrum voluptates exercitationem aspernatur autem tempora reprehenderit aperiam praesentium eligendi, aut illo minima facere omnis nisi.")
   ]
 
   constructor() { }
